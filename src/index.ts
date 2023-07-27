880981f43b72fc5297cc1877f203bd75075ffa51
whp98 <whp98@outlook.com>
2023-07-27 14:26:03 +0800

    修复语法错误

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,39 @@
-import { join } from "path";
-import { readdirSync, statSync, existsSync, readFileSync } from "fs";
-import { type DefaultTheme } from "vitepress";
-import { type Plugin, type ViteDevServer } from "vite";
-import type { SidebarPluginOptionType, UserConfig } from "./types";
+import { join } from 'path';
+import { existsSync, readdirSync, readFileSync, statSync } from 'fs';
+import { type DefaultTheme } from 'vitepress';
+import { type Plugin, type ViteDevServer } from 'vite';
+import type { SidebarPluginOptionType, UserConfig } from './types';
 
-import { DEFAULT_IGNORE_FOLDER, log, removePrefix } from "./utils";
+import { DEFAULT_IGNORE_FOLDER, log, removePrefix } from './utils';
 
 let option: SidebarPluginOptionType;
+
 // 尝试从一个md文件中读取标题，读取到第一个 ‘# 标题内容’ 的时候返回这一行
-function getTitleFromFile(realFileName: String) {
+function getTitleFromFile (realFileName: string): string | undefined {
   if (!existsSync(realFileName)) {
     return undefined;
   }
   const fileExtension = realFileName.substring(
-    realFileName.lastIndexOf(".") + 1
+    realFileName.lastIndexOf('.') + 1
   );
-  if (fileExtension != "md" && fileExtension != "MD") {
+  if (fileExtension !== 'md' && fileExtension !== 'MD') {
     return undefined;
   }
-  try {
-    // read contents of the file
-    const data = readFileSync(realFileName, "UTF-8");
-    // split the contents by new line
-    const lines = data.split(/\r?\n/);
-    // print all lines
-    for (let index = 0; index < lines.length; index++) {
-      const line = lines[index];
-      if (line.indexOf("# ") > -1) {
-        return line.replace("# ", "");
-      }
+  // read contents of the file
+  const data = readFileSync(realFileName, { encoding: 'utf-8' });
+  // split the contents by new line
+  const lines = data.split(/\r?\n/);
+  // print all lines
+  for (let index = 0; index < lines.length; index++) {
+    const line = lines[index];
+    if (line.includes('# ')) {
+      return line.replace('# ', '');
     }
-  } catch (err) {
-    throw err;
   }
+  return undefined;
 }
-function createSideBarItems(
+
+function createSideBarItems (
   targetPath: string,
   ...reset: string[]
 ): DefaultTheme.SidebarItem[] {
@@ -45,12 +44,12 @@ function createSideBarItems(
     sideBarItemsResolved,
     beforeCreateSideBarItems,
     ignoreList = [],
-    titleFromFile = false,
+    titleFromFile = false
   } = option;
   const rawNode = readdirSync(join(targetPath, ...reset));
   const node = beforeCreateSideBarItems?.(rawNode) ?? rawNode;
-  const currentDir = join(targetPath, ...reset)
-  if (ignoreIndexItem && node.length === 1 && node[0] === "index.md") {
+  const currentDir = join(targetPath, ...reset);
+  if (ignoreIndexItem && node.length === 1 && node[0] === 'index.md') {
     return [];
   }
   const result: DefaultTheme.SidebarItem[] = [];
@@ -64,25 +63,26 @@ function createSideBarItems(
       const items = createSideBarItems(join(targetPath), ...reset, fname);
       // replace directory name, if yes
       let text = fname;
-      //读取文件夹根目录的index.md中的标题,作为文件夹的标题
-      const title1 = getTitleFromFile(join(currentDir,fname,'index.md'))
-      const title2 = getTitleFromFile(join(currentDir,fname,'index.MD'))
-      const title3 = getTitleFromFile(join(currentDir,fname,fname+".md"))
-      if(title1){
-        text = title1
-      }else if(title2){
-        text = title2
-      }else if(title3){
-        text = title3
+      // 读取文件夹根目录的index.md中的标题,作为文件夹的标题
+      if (titleFromFile) {
+        const title1 = getTitleFromFile(join(currentDir, fname, 'index.md'));
+        const title2 = getTitleFromFile(join(currentDir, fname, 'index.MD'));
+        const title3 = getTitleFromFile(join(currentDir, fname, fname + '.md'));
+        if (title1) {
+          text = title1;
+        } else if (title2) {
+          text = title2;
+        } else if (title3) {
+          text = title3;
+        }
       }
       if (deletePrefix) {
         text = removePrefix(text, deletePrefix);
       }
-
       if (items.length > 0) {
         const sidebarItem: DefaultTheme.SidebarItem = {
           text,
-          items,
+          items
         };
         // vitePress sidebar option collapsed
         sidebarItem.collapsed = collapsed;
@@ -91,24 +91,26 @@ function createSideBarItems(
     } else {
       // is filed
       if (
-        (ignoreIndexItem && fname === "index.md") ||
+        (ignoreIndexItem && fname === 'index.md') ||
         /^-.*\.(md|MD)$/.test(fname)
       ) {
         continue;
       }
-      const fileName = fname.replace(/\.md$/, "");
+      const fileName = fname.replace(/\.md$/, '');
       let text = fileName;
       if (deletePrefix) {
         text = removePrefix(text, deletePrefix);
       }
-      const realFileName = join(currentDir,fname)
-      const title = getTitleFromFile(realFileName)
-      if(title){
-        text = title
+      const realFileName = join(currentDir, fname);
+      if (titleFromFile) {
+        const title = getTitleFromFile(realFileName);
+        if (title) {
+          text = title;
+        }
       }
       const item: DefaultTheme.SidebarItem = {
         text,
-        link: "/" + [...reset, `${fileName}.html`].join("/"),
+        link: '/' + [...reset, `${fileName}.html`].join('/')
       };
       result.push(item);
     }
@@ -116,19 +118,23 @@ function createSideBarItems(
   return sideBarItemsResolved?.(result) ?? result;
 }
 
-function createSideBarGroups(
+function createSideBarGroups (
   targetPath: string,
   folder: string
 ): DefaultTheme.SidebarItem[] {
   return [
     {
-      items: createSideBarItems(targetPath, folder),
-    },
+      items: createSideBarItems(targetPath, folder)
+    }
   ];
 }
 
-function createSidebarMulti(path: string): DefaultTheme.SidebarMulti {
-  const { ignoreList = [], ignoreIndexItem = false, sideBarResolved } = option;
+function createSidebarMulti (path: string): DefaultTheme.SidebarMulti {
+  const {
+    ignoreList = [],
+    ignoreIndexItem = false,
+    sideBarResolved
+  } = option;
   const il = [...DEFAULT_IGNORE_FOLDER, ...ignoreList];
   const data: DefaultTheme.SidebarMulti = {};
   const node = readdirSync(path).filter(
@@ -153,36 +159,39 @@ function createSidebarMulti(path: string): DefaultTheme.SidebarMulti {
   return sideBarResolved?.(data) ?? data;
 }
 
-export default function VitePluginVitePressAutoSidebar(
+export default function VitePluginVitePressAutoSidebar (
   opt: SidebarPluginOptionType = {}
 ): Plugin {
   return {
-    name: "vite-plugin-vitepress-auto-sidebar",
-    configureServer({ watcher, restart }: ViteDevServer) {
-      const fsWatcher = watcher.add("*.md");
-      fsWatcher.on("all", async (event, path) => {
-        if (event !== "change") {
+    name: 'vite-plugin-vitepress-auto-sidebar',
+    configureServer ({
+      watcher,
+      restart
+    }: ViteDevServer) {
+      const fsWatcher = watcher.add('*.md');
+      fsWatcher.on('all', async (event, path) => {
+        if (event !== 'change') {
           log(`${event} ${path}`);
           try {
             await restart();
-            log("update sidebar...");
+            log('update sidebar...');
           } catch {
             log(`${event} ${path}`);
-            log("update sidebar failed");
+            log('update sidebar failed');
           }
         }
       });
     },
-    config(config) {
+    config (config) {
       option = opt;
-      const { path = "/docs" } = option;
+      const { path = '/docs' } = option;
       // increment ignore item
       const docsPath = join(process.cwd(), path);
       // create sidebar data and insert
       (config as UserConfig).vitepress.site.themeConfig.sidebar =
         createSidebarMulti(docsPath);
-      log("injected sidebar data successfully");
+      log('injected sidebar data successfully');
       return config;
-    },
+    }
   };
 }
--- src/types.ts
+++ src/types.ts
@@ -6,8 +6,8 @@ export interface SidebarPluginOptionType {
   ignoreIndexItem?: boolean
   deletePrefix?: string | RegExp
   collapsed?: boolean
-  //是否从文件获取sidebar标题，默认是否
-  titleFromFile?:boolean
+  // 是否从文件获取sidebar标题，默认是否
+  titleFromFile?: boolean
   sideBarResolved?: (data: DefaultTheme.SidebarMulti) => DefaultTheme.SidebarMulti
   sideBarItemsResolved?: (data: DefaultTheme.SidebarItem[]) => DefaultTheme.SidebarItem[]
   beforeCreateSideBarItems?: (data: string[]) => string[]
